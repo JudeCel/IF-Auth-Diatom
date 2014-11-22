ca4cc4c4d70cf0633a645ecae8e15fcf51d0cbb8
Takhir Mamirov <takhirm@diatomenterprises.com>
2014-11-22 17:44:14 +0200

    Updated session expiration.

--- handlers/expireSession.js
+++ handlers/expireSession.js
@@ -1,16 +1,19 @@
 "use strict";
 var _ = require('lodash');
 var ifData = require('if-data'), db = ifData.db;
+var ifCommon = require('if-common')
+var encryptor = ifCommon.utils.dotNetEncryptionHelper;
 var Q = require('q');
 
-function expireSession(params) {
+function expireSession(sessionIdEncrypted) {
+	var sessId = encryptor.decryptNumberFromUrl(sessionIdEncrypted);
 	var sql = "UPDATE sess \
 			SET expires = NOW(),\
 			deleted = NOW(), \
 			status = 123000200 \
 			WHERE id = ?";
 
-	return Q.nfcall(db.query, sql, [params.sessionId]);
+	return Q.nfcall(db.query, sql, [sessId]);
 }
 
 module.exports = expireSession;
